e7ba89468ee594d0cead3ff2021ae5a2d8343ff5
dinglevin <dingweiqiong@gmail.com>
2024-06-19 16:50:48 +0800

    完善leetcode 001

--- src/leetcode/number001.py
+++ src/leetcode/number001.py
@@ -8,3 +8,34 @@ class Solution:
                 if (nums[i] + nums[j] == target):
                     return [i, j]
         return [0, 0]
+
+    def two_sum_v2(nums: List[int], target: int) -> List[int]:
+        map = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            comp_index = map.get(complement)
+            if comp_index is not None:
+                return [comp_index, i]
+            map[nums[i]] = i
+
+        return [0, 0]
+
+    def two_sum_v3(nums: List[int], target: int) -> List[int]:
+        sorted_nums = sorted(nums)
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            sum = sorted_nums[left] + sorted_nums[right]
+            if sum == target:
+                return [nums.index(sorted_nums[left]),
+                        next(i for i in range(len(nums) - 1, -1, -1)
+                             if nums[i] == sorted_nums[right])
+                        ]
+            elif sum < target:
+                left += 1
+            else:
+                right -= 1
+
+        return [0, 0]
--- tests/leetcode/test_number001.py
+++ tests/leetcode/test_number001.py
@@ -5,6 +5,18 @@ from src.leetcode.number001 import Solution
 class TestSolution(unittest.TestCase):
     def test_two_sum_v1(self):
         self.assertEqual([0, 1], Solution.two_sum_v1([2, 7, 11, 15], 9))
+        self.assertEqual([1, 2], Solution.two_sum_v1([3, 2, 4], 6))
+        self.assertEqual([0, 1], Solution.two_sum_v1([3, 3], 6))
+
+    def test_two_sum_v2(self):
+        self.assertEqual([0, 1], Solution.two_sum_v2([2, 7, 11, 15], 9))
+        self.assertEqual([1, 2], Solution.two_sum_v2([3, 2, 4], 6))
+        self.assertEqual([0, 1], Solution.two_sum_v2([3, 3], 6))
+
+    def test_two_sum_v3(self):
+        self.assertEqual([0, 1], Solution.two_sum_v3([2, 7, 11, 15], 9))
+        self.assertEqual([1, 2], Solution.two_sum_v3([3, 2, 4], 6))
+        self.assertEqual([0, 1], Solution.two_sum_v3([3, 3], 6))
 
 
 if __name__ == '__main__':
